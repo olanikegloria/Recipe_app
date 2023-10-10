9473a62628bba6b4e01110ead45013737612accf
yemidada <yemidada@gmail.com>
2023-10-10 18:27:53 +0100

    Fix linter errors

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,39 @@
+class RecipesController < ApplicationController
+  before_action :set_recipe, only: %i[show destroy]
+
+  def index
+    @recipes = Recipe.all
+  end
+
+  def show
+    # show
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to @recipe, notice: 'Recipe was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @recipe.destroy
+    redirect_to recipes_url, notice: 'Recipe was successfully destroyed.'
+  end
+
+  private
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+  end
+end
