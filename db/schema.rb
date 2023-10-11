6dbbb30341b9192bb0bf04a628ae52e7b8976ac7
yemidada <yemidada@gmail.com>
2023-10-11 10:22:29 +0100

    Update device issue

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_10_164833) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_11_091046) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "recipes", force: :cascade do |t|
     t.string "name"
     t.integer "preparation_time"
@@ -19,7 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_10_164833) do
     t.boolean "public"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
