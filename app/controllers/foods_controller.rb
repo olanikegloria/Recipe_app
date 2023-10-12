7f04c6814fc61fbc4041586c5f1694a3aa196489
yemidada <yemidada@gmail.com>
2023-10-12 14:25:38 +0100

    Fix linter errors

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,43 +1,42 @@
 class FoodsController < ApplicationController
-    before_action :set_food, only: %i[show destroy]
-    before_action :authenticate_user!
-  
-    def index
-      @foods = Food.all
+  before_action :set_food, only: %i[show destroy]
+  before_action :authenticate_user!
+
+  def index
+    @foods = Food.all
+  end
+
+  def show
+    # show
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.new(food_params)
+    @food.user_id = current_user.id
+    if @food.save
+      redirect_to foods_path
+    else
+      render :new
     end
-  
-    def show
-      # show
-    end
-  
-    def new
-      @food = Food.new
-    end
-  
-    def create
-      @food = Food.new(food_params)
-      @food.user_id = current_user.id
-      if @food.save
-        redirect_to foods_path
-      else
-        render :new
-      end
-    end
-  
-    def destroy
-      @food = Food.find(params[:id])
-      @food.destroy
-      redirect_to foods_path, notice: 'food was successfully deleted.'
-    end
-  
-    private
-  
-    def set_food
-      @food = Food.find(params[:id])
-    end
-  
-    def food_params
-      params.require(:food).permit(:name, :preparation_time, :cooking_time, :description, :public)
-    end
+  end
+
+  def destroy
+    @food = Food.find(params[:id])
+    @food.destroy
+    redirect_to foods_path, notice: 'food was successfully deleted.'
+  end
+
+  private
+
+  def set_food
+    @food = Food.find(params[:id])
+  end
+
+  def food_params
+    params.require(:food).permit(:name, :preparation_time, :cooking_time, :description, :public)
+  end
 end
-  
